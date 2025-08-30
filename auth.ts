52bcb4b1ac0d381d75fa88019b4a4d5bc169868a
muhammed rashid <120827047+Mraashii@users.noreply.github.com>
2025-08-30 13:13:49 +0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -3,5 +3,6 @@ import { authConfig } from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: "jwt" },
+  secret: process.env.NEXTAUTH_SECRET,   // 🔑 add this line
   ...authConfig,
 })
